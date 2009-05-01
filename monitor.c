f9e91234855c4a46101e6c62b50f19eccb5c72ca
Mikael Høilund <mikael@hoilund.org>
2009-05-01 18:05:15 +0200

    Added ability to track multiple files.
    Also added `depend` helper script. Yummy.

--- monitor.c
+++ monitor.c
@@ -5,14 +5,16 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-	char *path = argv[1];
+	int path_count = argc - 1;
+	char **paths = &argv[1];
+	int event_fds[path_count];
+	struct kevent events_to_monitor[path_count];
+	
 	int queue;
-	int event_fd;
-	struct kevent events_to_monitor;
 	struct kevent event_data;
 	
-	if (argc != 2) {
-		fprintf(stderr, "Usage: %s <file_path>\n", argv[0]);
+	if (argc < 2) {
+		fprintf(stderr, "Usage: %s <file_path> …\n", argv[0]);
 		return 1;
 	}
 	
@@ -23,17 +25,20 @@ int main(int argc, char *argv[]) {
 	}
 	
 	/* Open a file descriptor for event reading. */
-	event_fd = open(path, O_EVTONLY);
-	if (event_fd <= 0) {
-		fprintf(stderr, "The file %s could not be opened for monitoring: '%s'.\n", path, strerror(errno));
-		return 1;
+	int i;
+	for(i = 0; i < path_count; ++i) {
+		event_fds[i] = open(paths[i], O_EVTONLY);
+		if (event_fds[i] <= 0) {
+			fprintf(stderr, "The file %s could not be opened for monitoring: '%s'.\n", paths[i], strerror(errno));
+			return 1;
+		}
+		
+		/* Declare what we're waiting for. See kevent(2). */
+		EV_SET(&events_to_monitor[i], event_fds[i], EVFILT_VNODE, EV_ADD | EV_CLEAR, NOTE_ATTRIB, 0, NULL);
 	}
 	
-	/* Declare what we're waiting for. See kevent(2). */
-	EV_SET(&events_to_monitor, event_fd, EVFILT_VNODE, EV_ADD | EV_CLEAR, NOTE_ATTRIB, 0, NULL);
-	
 	/* Handle one (1) event for one (1) file. Waits indefinitely. */
-	int event_count = kevent(queue, &events_to_monitor, 1, &event_data, 1, NULL);
+	int event_count = kevent(queue, events_to_monitor, path_count, &event_data, 1, NULL);
 	
 	if ((event_count < 0) || (event_data.flags == EV_ERROR)) {
 		/* An error occurred. */
